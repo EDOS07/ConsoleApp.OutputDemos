4f26b47e74c268a95d04bf8172f34d266b3e9435
lalitomix77734 <lalitomix77734@gmail.com>
2024-03-20 17:22:49 -0600

    test

--- ConsoleApp.UserInputDemo/Program.cs
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -2,24 +2,41 @@
 
 string firstName = string.Empty;
 string lastName = string.Empty;
-int age = 0;
+int age;
 int retirementAge = 65;
+decimal salary;
+char gender;
+bool working = false;
 
 // Prompt the user for input
-Console.WriteLine("Plaese enter your first name");
+Console.WriteLine("Plaese enter your first name: ");
 firstName = Console.ReadLine();
 
-Console.WriteLine("Plaese enter your last name");
+Console.WriteLine("Plaese enter your last name: ");
 lastName = Console.ReadLine();
 
-Console.WriteLine("Plaese enter your age");
+Console.WriteLine("Plaese enter your age: ");
 age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Plaese enter your salary: ");
+salary = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Plaese enter your gender (M or F): ");
+gender = Convert.ToChar(Console.ReadLine());
+
+Console.WriteLine("Are you Workin? (true or false): ");
+working = Convert.ToBoolean(Console.ReadLine());
 
 // Proccess the data
 int workingYearsRemaining = retirementAge - age;
 
 // Output the Results to the user
-
+        
 Console.WriteLine($"Full name: {firstName} {lastName}");
 Console.WriteLine($"Age: {age}");
-Console.WriteLine($"Working yaers remaining: {workingYearsRemaining}");
+Console.WriteLine($"Your Salary is: {salary}");
+Console.WriteLine($"Your Gender is: {gender}");
+Console.WriteLine($"You Are Employed: {working}");
+Console.WriteLine($"Number of working years remaining: {workingYearsRemaining}");
+
+
