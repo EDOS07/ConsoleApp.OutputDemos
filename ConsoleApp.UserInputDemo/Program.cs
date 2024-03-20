ca1c7d3b9d866d331800857acabd347048e5c262
lalitomix77734 <lalitomix77734@gmail.com>
2024-03-20 15:27:58 -0600

    added first and last name promps

--- ConsoleApp.UserInputDemo/Program.cs
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -1,12 +1,16 @@
 ﻿// Declare variables
 
-string name = string.Empty;
+string firstName = string.Empty;
+string lastName = string.Empty;
 int age = 0;
 int retirementAge = 65;
 
 // Prompt the user for input
-Console.WriteLine("Plaese enter your name");
-name = Console.ReadLine();
+Console.WriteLine("Plaese enter your first name");
+firstName = Console.ReadLine();
+
+Console.WriteLine("Plaese enter your last name");
+lastName = Console.ReadLine();
 
 Console.WriteLine("Plaese enter your age");
 age = Convert.ToInt32(Console.ReadLine());
@@ -16,6 +20,6 @@ int workingYearsRemaining = retirementAge - age;
 
 // Output the Results to the user
 
-Console.WriteLine($"Full name: {name}");
+Console.WriteLine($"Full name: {firstName} {lastName}");
 Console.WriteLine($"Age: {age}");
 Console.WriteLine($"Working yaers remaining: {workingYearsRemaining}");
